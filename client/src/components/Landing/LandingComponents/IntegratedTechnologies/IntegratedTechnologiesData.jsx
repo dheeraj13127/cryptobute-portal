0c551c3baa9867be25b6306182c15b0e5a0d9da3
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2022-12-06 01:10:58 +0530

    added faq

--- client/src/components/Landing/LandingComponents/IntegratedTechnologies/IntegratedTechnologiesData.jsx
+++ client/src/components/Landing/LandingComponents/IntegratedTechnologies/IntegratedTechnologiesData.jsx
@@ -2,6 +2,11 @@ import ipfs from "../../../../assets/other/ipfs.png";
 import meta from "../../../../assets/other/meta.png";
 import node from "../../../../assets/other/node.png";
 import wallet from "../../../../assets/other/wallet.png";
+import push from "../../../../assets/other/push.jpg";
+import ens from "../../../../assets/other/ens.jpg";
+import biconomy from "../../../../assets/other/biconomy.png";
+import valist from "../../../../assets/other/valist.jpg";
+
 import "../../../../styles/LandingStyles/IntegratedTechnologies.scss";
 export const integratedTechnologies = [
   {
@@ -23,5 +28,25 @@ export const integratedTechnologies = [
     id: 4,
     url: meta,
     classname:"metaImg"
+  },
+  {
+    id: 5,
+    url: push,
+    classname:"pushImg"
+  },
+  {
+    id: 6,
+    url: ens,
+    classname:"pushImg"
+  },
+  {
+    id: 7,
+    url: biconomy,
+    classname:"pushImg"
+  },
+  {
+    id: 8,
+    url: valist,
+    classname:"pushImg"
   },
 ];
